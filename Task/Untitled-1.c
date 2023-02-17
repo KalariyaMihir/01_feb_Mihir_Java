505356276aaa93db0d365db10e25edc8b676f71c
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-02-17 12:11:37 +0530

    Add files via upload

---
+++ Task/Untitled-1.c
@@ -0,0 +1,9 @@
+// write a program to show home address
+#include<stdio.h>
+void main()
+// global variable
+{
+printf("Block no.20,street no.5");
+printf("Gayatri Park Socity, Nr.Marvel Hospital");
+printf("Rajkot Gujarat - 360004");
+}
