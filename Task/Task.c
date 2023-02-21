ab16a3bcdc161ffe1ae6f6a322122cf9e40a7748
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-02-21 13:59:52 +0530

    Add files via upload

---
+++ Task/Task.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    printf("Faculty - Nidhi Nadiyapara");
+    printf("\nStudent \b's Details ");
+    printf("\nStudent's Name \tCourse Name   \tAge \tGender \tFees");
+    printf("\nMihir    \tFUllstack-Java \t21\tMale\t90,000 ");
+    printf("\nHitesh   \tBackend-Python \t21\tMale\t60,000 \vFaiz      \tMernStack-NodeJS\b\t21\tOther\t90,000");
+    printf("\nRaj      \tC-C++   \t19\tMale\t25,000");
+    printf("\nMIHIR\r");
+
+    printf("               \\'Thank You Ma\edam'/");
+}
