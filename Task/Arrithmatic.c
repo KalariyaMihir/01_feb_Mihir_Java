92914901a2372b5b483d133855aecae75c51ae42
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-02-28 13:14:35 +0530

    Add files via upload

---
+++ Task/Arrithmatic.c
@@ -0,0 +1,27 @@
+// Arrithmatic operators
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    int ans;
+    char c='A',d='B';
+    float div;
+    printf("Enter Two Value:");
+    scanf("%d%d",&a,&b);
+    ans=a+b;
+    //printf("2 Number Addition : %d",ans);
+    printf("Addition of 2 Numbers  : %d",c+d);
+    ans=a-b;
+    printf("\nSubstration of 2 Numbers :%d",ans);
+    ans=a*b;
+    printf("\nMultiplication of 2 Numbers :%d",ans);
+    div=(float)a/(float)b;
+    //ans=a/b;
+    printf("\nDivision of 2 Numbers :%f",div);
+    ans=a%b;
+    printf("\nModulo of 2 Numbers :%d",ans);
+    ans=a++;
+    printf("\n\bIncrement of a :%d",a);
+    ans=b--;
+    printf("\n\bIncrment of b :%d",b);
+}
