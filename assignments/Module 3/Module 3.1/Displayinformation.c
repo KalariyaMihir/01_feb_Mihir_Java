ee9b4ed4c5ad17fa70fd51dba390b0d7491341a1
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-03-07 18:12:16 +0530

    Add files via upload

---
+++ assignments/Module 3/Module 3.1/Displayinformation.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    printf("My Name is :        KALARIYA MIHIR");
+    printf("\n\nMy Birth date is :  03/05/2001");
+    printf("\n\nMy Age is :         21 Years");
+    printf("\n\nMy Address is :     Last Block, Street no.5,");
+    printf("\n                    Gayatri Park Society, Nr.Marvel Hospital,");
+    printf("\n                    Gujarat - Rajkot, 360004.");
+}
