a5c03e69c6fec0962104d8408bbda906a62ec26c
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-03-07 22:42:39 +0530

    Add files via upload

---
+++ assignments/Module 3/Module 3.1/checkleapyear.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+    int Years;
+    printf("Enter Year :");
+    scanf("%d",&Years);
+    if (Years%400==0)
+    {
+        printf("%dThis Year is Leap Year",Years);
+    }
+    else if(Years % 100==0)
+    {
+        printf("%dThis Year is not Leap Year",Years);
+    }
+    else if (Years % 4==0)
+    {
+        printf("%d Year is a Leap year",Years);
+    }
+    
+    else
+    {
+        printf("%dThis Year is Not Leap Year");
+    }
+}
