26916af4281bdb116240ac82afbfce852365a86e
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-03-07 10:47:57 +0530

    Add files via upload

---
+++ Practice/BitwiseOperator.c
@@ -0,0 +1,16 @@
+//bitwise Operators &,!,|,~,<<,>>,^
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enter Two value :");
+    scanf("%d%d",&a,&b);
+    printf("\nBitwise And is:%d",a&b);
+    printf("\nBitwise Or is:%d",a|b);
+    printf("\nBitwise Left swifft is:%d",a<<b);
+    printf("\nBitwise Right Swift is:%d",a>>b);
+    printf("\nBitwise Not is:%d",!(a|b));
+    printf("\nBitwise compliment:%d",~a);
+    printf("\nBitwise XOR:%d",a^b);
+
+}                                      1
