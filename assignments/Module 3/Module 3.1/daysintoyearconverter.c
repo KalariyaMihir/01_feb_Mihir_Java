cba0545649ca5db327dda714325b9a66f0f0f520
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-03-08 14:20:42 +0530

    Add files via upload

---
+++ assignments/Module 3/Module 3.1/daysintoyearconverter.c
@@ -0,0 +1,12 @@
+// WAP to convert Days into Years
+#include<stdio.h>
+void main()
+{
+    float days;
+    float years;
+    printf("Enter Days : ");
+    scanf("%f",&days);
+    years=(float)days/(float)365;
+    printf("Days Equals to '%f' Years",years);
+
+}
---
+++ assignments/Module 3/Module 3.1/yearsintodaysconverte.c
@@ -0,0 +1,11 @@
+//WAP to convert Years into Days 
+#include<stdio.h>
+void main()
+{
+    float years;
+    float days;
+    printf("Enter Years : ");
+    scanf("%f",&years);
+    days=years*365.2425;
+    printf("Years Equals to '%f' Days",days);
+}
