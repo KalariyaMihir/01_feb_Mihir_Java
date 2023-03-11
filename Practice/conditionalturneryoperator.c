bcaa8cc3ac6cf3c9be5fe240a76f9aade3f8c353
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-03-11 12:06:42 +0530

    Add files via upload

---
+++ Practice/conditionalturneryoperator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enter the value of two number : ");
+    scanf("%d%d",&a,&b);
+    (a==b) ? (printf("Both are Equal")) : (printf("both are not Equal"));
+    
+}
