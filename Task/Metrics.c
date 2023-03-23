871adc2f84bb3794b4b1b38acecaffb5ecbdb238
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-03-23 10:58:23 +0530

    Update Metrics.c

--- Task/Metrics.c
+++ Task/Metrics.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 int main()
 {
-    int number[3][3]; //= {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int number[3][3]; // = {{1,2,3},{4,5,6},{7,8,9}};
     int i, j;
     for (i = 0; i < 3; i++)
     {
