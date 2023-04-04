b55d758309d3b542c2bdf184080e658dfda6f7bc
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-04-04 11:52:43 +0530

    Add files via upload

---
+++ assignments/Module 3/Module 3.2/Maxnumberfind.c
@@ -0,0 +1,12 @@
+// WAP to find Max Number From Given 
+ 
+ #include<stdio.h>
+ int main()
+ {
+    int a,b;
+    printf("Enter First Value :");
+    scanf("%d",&a);
+    printf("Enter Second Value :");
+    scanf("%d",&b);
+    (a>b) ? (printf("First Value is MAx")) : (printf("Second Value is Max"));
+ }
