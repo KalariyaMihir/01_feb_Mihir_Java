294439497ef02819e996d58edf5852d0199fda9c
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-04-11 13:11:37 +0530

    Add files via upload

---
+++ C++/Practice/hello.cpp
@@ -0,0 +1,16 @@
+// WAP to Get Value & print in c++
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[20];  
+    int no;
+    cout<< "Enter Your Name :";
+    cin>>name;
+    cout<<" Enter Your ID : ";
+    cin>>no;
+    cout<< name<<" your id is " <<no ;
+
+    return 0;
+}
