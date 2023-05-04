3f4aec408dc6b87ae7498fe165de6cba80e42e98
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-05-04 16:51:35 +0530

    Update FoodBillingSystem.c

--- Assessment/Module 1/FoodBillingSystem.c
+++ Assessment/Module 1/FoodBillingSystem.c
@@ -18,15 +18,15 @@ void main()
 
     printf("\n\n -- So What Would you like to Have ? --    ");
 
-    // Do while loop for condition check 
+    // Do while loop for condition check
     do
     {
-    
+
     // select item from menu
     printf("\n\nSelect Item from Menu :");
     scanf("%d",&item); 
     // Enter quantity as per users choice
-    printf("Enter Quantity :");
+    printf("Select Quantity :");
     scanf("%d",&quantity);
 
     // switch for choice item
@@ -37,46 +37,46 @@ void main()
 
         // cost = item cost multiple to quantity
         cost = 150 * quantity;
-        printf("%d Pizza",quantity);
+        printf("%d Pizza ",quantity);
         break;
 
     case 2:
         cost = 70 * quantity;
-        printf("%d Burger",quantity);    
+        printf("%d Burger ",quantity);    
         break;
 
     case 3:
         cost = 90 * quantity;
-        printf("%d Pasta",quantity);
+        printf("%d Pasta ",quantity);
         break;
 
     case 4:
         cost = 50 * quantity;
-        printf("%d  Fries",quantity);
+        printf("%d Fries ",quantity);
         break;
 
     case 5:
         cost = 25 * quantity;
-        printf("%d Cold Drinks",quantity);     
+        printf("%d Cold Drinks ",quantity);     
         break;   
 
     default:
         printf("--Sorry That Item is not Available --");
         break;
     }
+
     
     // total bill = addition of all ordered item cost
     totalbill += cost;
 
     // to order something more
     printf("\n\nDo you Want to Order something More ? ");
-    printf("Press Y / N : ");
-    scanf("%c",&more);
+    printf("Press Y / N ");
+    scanf(" %c",&more);
     
     }
     while (more == 'Y' || more == 'y' );
 
-   // for print total bill 
-    printf("\nYour Total Bill is :%d",totalbill);
+    // for print total bill
+    printf("\nYour Total Bill is : %d",totalbill);
 }
-
