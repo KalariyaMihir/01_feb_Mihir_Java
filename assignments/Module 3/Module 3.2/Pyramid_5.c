206fa427c2af43018d9603bcb950b9cf1ef30113
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-06-08 15:50:00 +0530

    Update Pyramid_5.c

--- assignments/Module 3/Module 3.2/Pyramid_5.c
+++ assignments/Module 3/Module 3.2/Pyramid_5.c
@@ -1,36 +1,40 @@
 /*
-WAP to Print :           * 
-                       * * * 
-                     * * * * * 
-                   * * * * * * * 
-                 * * * * * * * * * 
-               * * * * * * * * * * * 
- 
+WAP to Print :           *
+                       * * *
+                     * * * * *
+                   * * * * * * *
+                 * * * * * * * * *
+               * * * * * * * * * * *
+
 
 */
 
-#include<stdio.h>
- int main()
- {
-    int i,j,k = 0,rows;
+#include <stdio.h>
+int main()
+{
+  int i, j, k = 0, rows;
 
-    printf("Enter Number of Rows : ");
-    scanf("%d",&rows);
+  printf("Enter Number of Rows : ");
+  scanf("%d", &rows);
 
-    for ( i = 1; i <= rows; i++ , k = 0)
+  for (i = 0; i < rows; i++, k = 0)
+  {
+    for (j = 1; j <= rows - i; j++)
     {
-        for (j = 1; j <= rows - i ; j++)
-        {
-            printf("  ");
-        }
-        while (k != 2 * i - 1)
-        {
-            printf("* ");
-            k++;
-        }
-        
-        printf("\n");
-        
+      printf("  ");
     }
-    
- }
+    for (j = 0; j <= i; j++)
+    {
+      printf("* ");
+    }
+    for (j = 0; j <= i - 1; j++)
+    {
+      printf("* ");
+    }
+
+    {
+      printf("\n");
+      k++;
+    }
+  }
+}
