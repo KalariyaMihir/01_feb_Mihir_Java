70471f085cc53a4a835c469e801a6cc878ab73c5
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-06-08 16:25:14 +0530

    Update Swap2nowithoutusingthirdveriable.c

--- assignments/Module 3/Module 3.2/Swap2nowithoutusingthirdveriable.c
+++ assignments/Module 3/Module 3.2/Swap2nowithoutusingthirdveriable.c
@@ -1,12 +1,20 @@
 // Write a program to swap two numbers without using third variable
 #include<stdio.h>
-int main()
+
+int a,b;
+
+// void get() for Get the values from Users 
+void get()
 {
-    int a,b;
     printf("\nEnter First Value :");
     scanf("%d",&a);
     printf("\nEnter Second Value : ");
     scanf("%d",&b);
+}
+
+// Void print() for Calculate the values and print them 
+void print()
+{
     printf("\n \t%d + %d = %d",a,b,a+b);
     printf("\n \t%d - %d = %d",a,b,a-b);
     printf("\n \t%d / %d = %d",a,b,a/b);
@@ -14,4 +22,13 @@ int main()
     printf("\n \tIncrement of A : %d  ",++a);
     printf("\n \tDecrement of B : %d ",--b);
 
+}
+
+// main() to call both the function
+int main()
+{
+    get();
+    print();
+
+    return 0;
 }
