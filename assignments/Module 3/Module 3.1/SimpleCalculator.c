b700ad906ba4ae9604df2ebf920326a6ac0ec519
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-06-19 14:29:35 +0530

    Add files via upload

--- assignments/Module 3/Module 3.1/SimpleCalculator.c
+++ assignments/Module 3/Module 3.1/SimpleCalculator.c
@@ -7,14 +7,9 @@ void main()
     float div;
     printf("Enter Two Value:");
     scanf("%d%d",&a,&b);
-    ans=a+b;
-    printf("Addition of Two Numbers is :       %d",ans);
-    ans=a-b;
-    printf("\n\nSubstration of Two Numbers is :    %d",ans);
-    ans=a*b;
-    printf("\n\nMultiplication of Two Numbers is : %d",ans);
-    div=(float)a/(float)b;
-    printf("\n\nDivision of Two Numbers is :       %f",div);
-    ans=a%b;
-    printf("\n\nModulo of Two Numbers is :         %d",ans);
+    printf("Addition of Two Numbers is :       %d",a+b);
+    printf("\n\nSubstration of Two Numbers is :    %d",a-b);
+    printf("\n\nMultiplication of Two Numbers is : %d",a*b);
+    printf("\n\nDivision of Two Numbers is :       %f",a/b);
+    printf("\n\nModulo of Two Numbers is :         %d",a%b);
 } 
