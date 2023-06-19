82f8672c3304d051408e822fe840ac31e6918f75
Mihir Kalariya <124235902+KalariyaMihir@users.noreply.github.com>
2023-06-19 14:25:11 +0530

    Update FindAareaofCircle.c

--- assignments/Module 3/Module 3.1/FindAareaofCircle.c
+++ assignments/Module 3/Module 3.1/FindAareaofCircle.c
@@ -1,13 +1,12 @@
 // WAP to Find Area of Circle
 #include<stdio.h>
+#define pi 3.14
 void main()
 {
-    float pie;
     float area;
     float radius;
-    pie = 3.14;
     printf("Enter Radius of Circle : ");
     scanf("%f",&radius);
-    area =pie*radius*radius; 
+    area =pi*radius*radius; 
     printf("Total Area of Circle is :%f",area);
 }
